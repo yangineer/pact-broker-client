1ea9588d6688cbab92f1595554d05d7025b37c77
Miguel Beltre <miguel.beltre@babylonhealth.com>
2017-06-01 14:29:43 +0100

    fix tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,3 +1,5 @@
+import random
+
 from pact_client.commands import pull_contract, push_contract
 
 
@@ -10,13 +12,18 @@ def test_pull_contract():
     assert response.status_code == 200
 
 
-def test_put_contract():
+def test_push_contract():
+    def random_version():
+        d1 = int(random.random() * 100)
+        d2 = int(random.random() * 100)
+        d3 = int(random.random() * 100)
+        return f'{d1}.{d2}.{d3}'
+
     response = push_contract(
         broker_url='http://localhost:8080/',
         provider='Animal Service',
         consumer='Zoo App',
         contract_path='tests/contract.json',
-        version='1'
-
+        version=f'{random_version()}'
     )
     assert response.status_code == 201
