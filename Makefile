217afe208564b20eaf4b547008a779326dc08ca3
Miguel Beltre <miguel.beltre@babylonhealth.com>
2017-06-02 11:58:59 +0100

    added make command for testing

---
+++ Makefile
@@ -0,0 +1,6 @@
+NAME=pact_broker_client
+VERSION=$(shell cat VERSION)
+
+
+test:
+	pytest
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,23 +1,46 @@
+import json
+import os
+
+from mock import patch
+from requests import Response
+
 from random import randint
 
-from pact_client.commands import pull_contract, push_contract
+from broker_client.commands import pull_contract, push_contract
 
 
-def test_pull_contract():
-    response = pull_contract(
-        broker_url='http://localhost:8080/',
-        provider='Animal Service',
-        consumer='Zoo App'
-    )
-    assert response.status_code == 200
+@patch('broker_client.commands.requests.get')
+def test_pull_contract(mock_get):
+    provider = 'Animal Service'
+    consumer = 'Zoo App'
 
+    mocked_response = Response()
+    mocked_response.status_code = 200
+    mocked_response._content = json.dumps(
+        {'consumer': consumer, 'provider': provider}
+    ).encode()
+    mock_get.return_value = mocked_response
 
-def test_push_contract():
-    response = push_contract(
+    pull_contract(
         broker_url='http://localhost:8080/',
         provider='Animal Service',
         consumer='Zoo App',
-        contract_path='tests/contract.json',
-        version=f'{randint(100, 100000)}'
     )
-    assert response.status_code == 201
+
+    saved_pact_path = f'./{provider}_{consumer}.json'.lower().replace(' ', '_')
+    assert os.path.exists(saved_pact_path)
+
+
+@patch('broker_client.commands.requests.put')
+def test_push_contract(mock_put):
+    mocked_response = Response()
+    mocked_response.status_code = 201
+    mock_put.return_value = mocked_response
+    push_contract(
+        broker_url='http://localhost:8080/',
+        provider='Animal Service',
+        consumer='Zoo App',
+        pact_file='tests/stubs/contract.json',
+        version=f'{randint(100, 100000)}'
+    )[0]
+    assert mock_put.called
