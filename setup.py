f63271b622f9f591424eb97f496b24c58b55cee9
Richard O'Dwyer <richard@richard.do>
2017-06-08 11:32:49 +0100

    Back to development: 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='pact-broker-client',
-    version='0.1.0',
+    version='0.1.1.dev0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=install_requires,
